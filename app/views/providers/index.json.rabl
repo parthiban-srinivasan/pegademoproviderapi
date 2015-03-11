03a45e4754b213348ed45405f1de8d86d5a8cca4
parthi <parthiban.srinivasan@gmail.com>
2015-03-11 08:32:30 +0000

    few more changes

--- app/views/providers/index.json.rabl
+++ app/views/providers/index.json.rabl
@@ -1,2 +1,2 @@
-collection @providers
+objects @providers
 attributes :id, :provider_num, :routing_num, :bank_account_num, :eft_ind, :bulking_ind, :suppression_ind, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   #constraints subdomian: 'api' do
   #namespace :api, path: '/', defaults: { format: :xml } do
-  resources :providers, except: [:new, :edit], defaults: { format: :xml }
+  resources :providers, except: [:new, :edit], defaults: { format: :json }
   #end
   #end
   
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+  #root 'provider#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
